49bb76ba70e38490587f8448137a60de448453d1
h1nadesu <k1hyxn01@naver.com>
2025-03-13 14:47:25 +0900

    Ready for Deploy

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:netflix/screen/home_screen.dart';
+import 'package:netflix/screen/more_screen.dart';
+import 'package:netflix/widget/bottom_bar.dart';
+import 'package:netflix/screen/search_screen.dart';
+import 'package:netflix/screen/like_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ZzapFlix',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        colorScheme: ColorScheme.dark(secondary: Colors.white),
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomeScreen(),
+              SearchScreen(),
+              LikeScreen(),
+              MoreScreen(),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
